51e55ade12014258e982263f50c78dea5627eb00
henriquexvr <henriquecaldeirax3@gmail.com>
2025-07-29 00:32:22 -0300

    Alteração no exercicio Matriz Diagonal

--- src/academy/devdojo/arrays/multidimensionais/DiagonalMatriz.java
+++ src/academy/devdojo/arrays/multidimensionais/DiagonalMatriz.java
@@ -18,7 +18,7 @@ public class DiagonalMatriz {
         //Mostra a matriz para o usuario
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz.length; j++) {
-                System.out.print(matriz[i][j] + "    ");
+                System.out.print(matriz[i][j] + " ");
             }
             System.out.println(" ");
         }
